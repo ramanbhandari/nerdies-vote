146e1c75406e1b8968899c8aa7ad3d2e0a2f4bd4
ramanbhandari <ramanbhandarieoes@gmail.com>
2024-11-03 20:10:34 -0600

    Added get count, add candidate and only admin modifiers implementation

--- smart-contracts/contracts/Voting.sol
+++ smart-contracts/contracts/Voting.sol
@@ -12,17 +12,21 @@ contract Voting {
     mapping(uint => Candidate) public candidates;
     mapping(bytes32 => bool) public isEligible;
     mapping(bytes32 => bool) public hasVoted;
+    uint public candidatesCount;
 
     // log key actions
     event VoterRegistered(bytes32 voterHash);
     event VoteCasted(uint candidateId, bytes32 voterHash);
+    event CandidateAdded(uint candidateId, string name);
 
+    address public admin;
     constructor() {
-
+        admin = msg.sender;
     }
 
     modifier onlyAdmin() {
-
+        require(msg.sender == admin, "Not authorized");
+        _;
     }
 
     // register the voter, only admins should be able to access
@@ -54,12 +58,18 @@ contract Voting {
     }
 
     // get the voter counts for all candidates - Public
-    function getCount() public view returns (uint[] counts) {
-
+    function getCount() public view returns (uint[] memory) {
+        uint[] memory voteCounts = new uint[] (candidatesCount);
+        for (uint i = 1; i<= candidatesCount; i++) {
+            voteCounts[i-1] = candidates[i].voteCount;
+        }
+        return voteCounts;
     }
 
     // to add a candidate - Admin Only access
     function addCandidate(string memory _name) public onlyAdmin {
-
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        emit CandidateAdded(candidatesCount, _name);
     }
 }
