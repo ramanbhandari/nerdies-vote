d3d37785d6985c3b5edf2fc4489254cdc79814de
Anmolpreet Khangura <109428188+AnmolKhangura@users.noreply.github.com>
2024-11-10 15:32:12 -0600

    connecting to the contract

--- microservices/voter-registration/main.go
+++ microservices/voter-registration/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log"
+	"math/big"
+	"voter_registration/voting"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -13,6 +16,31 @@ func main() {
         log.Fatalf("Failed to connect to the local Ganache blockchain: %v", err)
     }
     log.Println("Connected to Ganache")
+
+
+	// Contract address 
+	contractAddress := common.HexToAddress("0xB248Fd56EdC2896F14BD4a7a539750870880a157")
+
+	// Call the interaction function
+	interactWithContract(client, contractAddress)
+
+
     defer client.Close()
 	
+}
+
+func interactWithContract(client *ethclient.Client, contractAddress common.Address) {
+	// getting the instance of the contract 
+    instance, err := voting.NewVoting(contractAddress, client)
+    if err != nil {
+        log.Fatalf("Failed to load contract instance: %v", err)
+    }
+
+    // call contract methods directly
+    result, err := instance.Candidates(nil,big.NewInt(1)) 
+    if err != nil {
+        log.Fatalf("Failed to call contract function: %v", err)
+    }
+
+    log.Println("Function result:", result)
 }
