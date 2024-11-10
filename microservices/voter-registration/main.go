d4a58711e92a9b7193f193a9210b24653a92eeae
Anmolpreet Khangura <109428188+AnmolKhangura@users.noreply.github.com>
2024-11-10 10:34:57 -0600

    connecting to the ganache

--- microservices/voter-registration/main.go
+++ microservices/voter-registration/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
 
 func main() {
-	fmt.Println("Hello world from Voter Registration Nerdiess")
+    // Connect to Ganache
+    client, err := ethclient.Dial("http://127.0.0.1:7545")
+    if err != nil {
+        log.Fatalf("Failed to connect to the local Ganache blockchain: %v", err)
+    }
+    log.Println("Connected to Ganache")
+    defer client.Close()
+	
 }
