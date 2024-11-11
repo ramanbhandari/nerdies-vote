e547bc2ef249d3655db3b9f810c3500fb430e52f
Anmolpreet Khangura <109428188+AnmolKhangura@users.noreply.github.com>
2024-11-11 12:31:03 -0600

    added a registerVoter func to register voters

--- microservices/voter-registration/main.go
+++ microservices/voter-registration/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"crypto/sha256"
 	"log"
 	"math/big"
-	"voter_registration/voting"
+	"time"
+	"voter_registration/pkg/auth"
+	"voter_registration/pkg/voting"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -17,19 +23,27 @@ func main() {
     }
     log.Println("Connected to Ganache")
 
+	//create an auth object to sign transactions 
+	auth, err := auth.NewAuth()
+    if err != nil {
+        log.Fatalf("Failed to create auth object: %v", err)
+    }
+
+	log.Println(auth.From)
 
 	// Contract address 
 	contractAddress := common.HexToAddress("0xB248Fd56EdC2896F14BD4a7a539750870880a157")
 
 	// Call the interaction function
-	interactWithContract(client, contractAddress)
+	checkCandidateTest(client, contractAddress)
+	registerVoter(auth, client, contractAddress)
 
 
     defer client.Close()
 	
 }
 
-func interactWithContract(client *ethclient.Client, contractAddress common.Address) {
+func checkCandidateTest(client *ethclient.Client, contractAddress common.Address) {
 	// getting the instance of the contract 
     instance, err := voting.NewVoting(contractAddress, client)
     if err != nil {
@@ -42,5 +56,53 @@ func interactWithContract(client *ethclient.Client, contractAddress common.Addre
         log.Fatalf("Failed to call contract function: %v", err)
     }
 
-    log.Println("Function result:", result)
+    log.Println("Function result:", result.VoteCount)
+}
+
+func registerVoter(auth *bind.TransactOpts,client *ethclient.Client, contractAddress common.Address) {
+	// getting the instance of the contract 
+    instance, err := voting.NewVoting(contractAddress, client)
+    if err != nil {
+        log.Fatalf("Failed to load contract instance: %v", err)
+    }
+
+	voteHash := sha256.Sum256([]byte("new voter 5"))
+	
+    // call register vote function on the contract
+    result, err := instance.RegisterVoterHash(auth, voteHash)
+    if err != nil {
+        log.Fatalf("Failed to call contract function: %v", err)
+    }
+
+    log.Println("Voter Reg result:", result.Hash().Hex())
+
+	// wait for the transaction receipt in a blocking manner
+	receipt, err := waitForReceipt(client, result.Hash())
+	if err != nil {
+		log.Fatalf("Failed to get transaction receipt: %v", err)
+	}
+
+    log.Println("Voter Reg receipt:", receipt.Logs[0])
+
+
+}
+
+func waitForReceipt(client *ethclient.Client, txHash common.Hash) (*types.Receipt, error) {
+	// Loop until the transaction receipt is found
+	for {
+		// Try to get the receipt
+		receipt, err := client.TransactionReceipt(context.Background() , txHash)
+		if err != nil && err.Error() != "not found" {
+			// If the error is not "not found", return the error
+			return nil, err
+		}
+
+		// If receipt is not nil, transaction is mined
+		if receipt != nil {
+			return receipt, nil
+		}
+
+		// If not mined yet, wait before trying again
+		time.Sleep(2 * time.Second) // Wait for 2 seconds before polling again
+	}
 }
